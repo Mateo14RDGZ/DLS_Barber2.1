4f6652b032f8f14a5303a70748ddaae11b7b8bfb
Mateo <porongos84314@gmail.com>
2025-07-18 16:50:24 -0300

    Fix: Correct serverless function syntax
    - Change from ES6 export to CommonJS module.exports
    - Add package.json for API dependencies
    - Fix function structure for Vercel serverless

--- api/auth/register.js
+++ api/auth/register.js
@@ -9,7 +9,7 @@ const pool = new Pool({
     }
 });
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // Configurar CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
